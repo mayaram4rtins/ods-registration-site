35a1766a142684ae5d7e063db0d1df652e4b6b0b
Mayara Neves Martins <mmayara.nmartins@gmail.com>
2023-05-04 12:07:41 -0300

    Criação do .route de login com adição de mensagens de login bem sucedido. Além disso, foi ajustado o lugar dos arquivos no folder para interação com o flask.

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, session
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+app.secret_key = 'palavrasecreta'
 
-@app.route("/")
-def Home():
-    return render_template("html/Tela_de_login.html")
+@app.route('/')
+def index():
+    return render_template('Tela_de_login.html')
+
+@app.route('/login')
+def login():
+    return render_template('Tela_de_login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'senha' == request.form['password']:
+        session['usuario_logado'] = request.form['name']
+        flash(request.form['name'] + ' logou com sucesso!')
+        return redirect('/')
+    else:
+        flash(request.form['name'] + " não logado! Verifique suas credenciais.")
+        return redirect('/login')
 
 if __name__ == '__main__':
     app.run(debug=True)
