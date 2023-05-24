5c1c958669ba2f43cd0ebc95bf9d3bcda6ae6999
Fabrig0s <fabriciosilva1999@outlook.com.br>
2023-05-23 21:24:16 -0300

    Adicao de itens pre definidos nas tabelas de usuarios e projetos do banco de dados

--- prepara_banco.py
+++ prepara_banco.py
@@ -40,11 +40,10 @@ TABLES['Projetos'] = ('''
       `nome_projeto` varchar(100) NOT NULL,
       `id_user` int(10) NOT NULL,
       `descricao` varchar(100) NOT NULL,
-      `ods` varchar(14) NOT NULL,
       `telefone` bigint NOT NULL,
       `data_cadastro` date NOT NULL,
       `status_projeto` varchar(10) NOT NULL,
-      `tag` varchart (15) NOT NULL,
+      `tag` varchar (15) NOT NULL,
       PRIMARY KEY (`id_proj`)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
 
@@ -63,38 +62,43 @@ for tabela_nome in TABLES:
       else:
             print('Ok, tabela criada!')
 
-'''
+
 # inserindo usuarios
-usuario_sql = 'INSERT INTO usuario (nome, nickname, senha) VALUES (%s, %s, %s)'
+usuario_sql = 'INSERT INTO cadastro_usuario (nome, email, senha, telefone) VALUES (%s, %s, %s, %s)'
 usuarios = [
-      ("Bruno Divino", "BD", "alohomora"),
-      ("Camila Ferreira", "Mila", "paozinho"),
-      ("Guilherme Louro", "Cake", "python_eh_vida")
+      ("João Profano", "jojopo@hotmail.com", "jojofofo33", "5511987634590"),
+      ("Telconir", "teteco_surf@gmali.com", "surfevida54", "5511929387429"),
+      ("Yasuo Shigeru", "shigeru_yasuo@yahoo.com.br", "3297yat", "5511912968164")
 ]
 cursor.executemany(usuario_sql, usuarios)
 
-cursor.execute('select * from jogoteca.usuarios')
+cursor.execute('select * from ods-registration-site.cadastro_usuario')
 print(' -------------  Usuários:  -------------')
 for user in cursor.fetchall():
     print(user[1])
 
-# inserindo jogos
-jogos_sql = 'INSERT INTO jogos (nome, categoria, console) VALUES (%s, %s, %s)'
-jogos = [
-      ('Tetris', 'Puzzle', 'Atari'),
-      ('God of War', 'Hack n Slash', 'PS2'),
-      ('Mortal Kombat', 'Luta', 'PS2'),
-      ('Valorant', 'FPS', 'PC'),
-      ('Crash Bandicoot', 'Hack n Slash', 'PS2'),
-      ('Need for Speed', 'Corrida', 'PS2'),
-]
-cursor.executemany(jogos_sql, jogos)
+# inserindo projetos
+projetos_sql = 'INSERT INTO jogos (nome_projeto, id_user, descricao, telefone, data_cadastro, status_projeto, tag) VALUES (%s, %s, %s, %s, %s, %s)'
+projetos = [
+      ('Irrigamento Autosuficiente', 
+      'Modelo de irrigamento de plantas onde não há dispersão de água no processo por meio de uma estrutura metálica resistente á corrosão.',
+      '11998124683','2023-05-23 00:00:00', 'Em andamento', 'Sustentabilidade'),
 
-cursor.execute('select * from jogoteca.jogos')
-print(' -------------  Jogos:  -------------')
-for jogo in cursor.fetchall():
-    print(jogo[1])
-'''
+      ('Restringir acesso ás áraeas de mananceais', 
+      'Incentivo á criação de uma coleção de leis que detenham o acesso á pessoas não autorizadas ás areás atigindas.',
+      '1191698431','2023-05-24 00:00:00', 'Não iniciado', 'Sustentabilidade'),
+      
+      ('Detecção de possíveis áreas atigindas por deslizamento', 
+      'Criar aplicação que possibilite aos usuários reportarem áreas que estam em risco.',
+      '11998271974','2023-05-22 00:00:00', 'Não iniciado', 'Deslizamento'),
+]
+cursor.executemany(projetos_sql, projetos)
+
+cursor.execute('select * from ods-registration-site.cadastro_projetos')
+print(' -------------  Projetos:  -------------')
+for projeto in cursor.fetchall():
+    print(projeto[1])
+
 
 # commitando se não nada tem efeito
 
